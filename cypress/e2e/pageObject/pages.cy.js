816fa343a1f7fa18654681bdcbb7c763e85a4fdb
elzaujkanii <107105923+elzaujkanii@users.noreply.github.com>
2022-06-10 14:36:43 +0200

    Made tests for creating a poll

---
+++ cypress/e2e/pageObject/pages.cy.js
@@ -0,0 +1,60 @@
+class CreateAPoll {
+    
+    navigate() {
+        cy.clearCookies();
+        cy.visit('https://doodle.com/en/');
+    }
+
+    closeCookies(){
+        cy.get('#onetrust-accept-btn-handler').click();
+        cy.wait(3000);
+        cy.get('.module__StyledHeading1-sc-8k4x5k-21 ').contains('Professional scheduling made easy');
+        cy.get('#onetrust-accept-btn-handler').should('not.be.visible');
+        return this
+    }
+
+    newPoll(){
+        cy.get(".module__StyledCreateMenu-sc-8k4x5k-105").click()
+        cy.get('.OrganizationForm__section-heading').contains('Create group poll');
+        return this 
+    }
+
+    typeName(){
+        cy.get('input[name="organizerName"]').type("Your Name");
+    }
+
+    typeDesc(){
+        cy.get('.MeetingMetadata__description__textarea').type("Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
+    }
+
+    typeTitle(){    
+        cy.get('input[placeholder="What\'s the occasion?"]').type("Title Test");
+    }
+
+    location(){    
+        cy.get('input[placeholder="Where will this happen?"]').type("Kosovo");
+    }
+
+    selectCountry(){
+        cy.get('#text').click();
+    }
+
+    typeEmail(){
+        let min = 100;
+        let max = 20000;
+        let num;
+        function randomEmail() {
+            num = Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        randomEmail();
+        let email = `test${num}@gmail.com` 
+        
+        cy.get('input[name="organizerEmail"]').type(email);
+    }
+
+    dateButton(){
+        cy.get('.OptionNavigatorButton__forward').click();
+    }
+}
+
+export default CreateAPoll
---
+++ cypress/e2e/pollSkeleton.cy.js
@@ -0,0 +1,67 @@
+/// <reference  types="cypress"  />
+
+import CreateAPoll from "./pageObject/pages.cy.js"
+
+const closeCookies = new CreateAPoll();
+const creatingPoll = new CreateAPoll();
+const typingName = new CreateAPoll();
+const email = new CreateAPoll();
+const title = new CreateAPoll();
+const desc = new CreateAPoll();
+const country = new CreateAPoll();
+const selectedCountry = new CreateAPoll();
+
+describe("Create New Poll", function () {
+    it("Go to homepage and close cookies", function () {
+        closeCookies.navigate();
+        closeCookies.closeCookies();
+    });
+
+    it("Create a new poll", function(){
+        creatingPoll.newPoll();
+    });
+
+    it("Type your name", function(){
+        typingName.typeName();
+    });
+    
+    it("Write Email", function(){
+        email.typeEmail();
+    });
+
+    it("Write Something", function(){
+        title.typeTitle();
+    });
+
+    it("Write a description", function(){
+        desc.typeDesc();
+    });
+
+    it("Choose a country", function(){
+        country.location();
+    });
+
+    it("Select this country", function(){
+        selectedCountry.selectCountry();
+    });
+}); 
+
+
+
+afterEach(() => {
+    //Code to Handle the Sesssions in cypress.
+    //Keep the Session alive when you jump to another test
+    let str = [];
+    cy.getCookies().then((cook) => {
+        cy.log(cook);
+        for (let l = 0; l < cook.length; l++) {
+            if (cook.length > 0 && l == 0) {
+                str[l] = cook[l].name;
+                Cypress.Cookies.preserveOnce(str[l]);
+            } else if (cook.length > 1 && l > 1) {
+                str[l] = cook[l].name;
+                Cypress.Cookies.preserveOnce(str[l]);
+            }
+        }
+    })
+})
